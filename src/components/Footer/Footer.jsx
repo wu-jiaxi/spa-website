9af5bc143c86e0ad1d7669232fcf36f4b0bdda21
wu-jiaxi <dev.jiaxiwu@gmail.com>
2024-06-14 23:46:41 -0400

    footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import "./HotelDetails.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Plan Your Visit</p>
+          <p>Book Treatments</p>
+          <p>Shop</p>
+          <p>About</p>
+        </div>
+        <div>
+          <p>FAQ</p>
+          <p>Careers</p>
+          <p>Press</p>
+          <p>Contact</p>
+        </div>
+      </div>
+      <div>
+        <div>Logo</div>
+        <div>Open Daily</div>
+        <div>
+          <div>General</div>
+          <div>Booking</div>
+        </div>
+        <div>Soja Spa Club</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
