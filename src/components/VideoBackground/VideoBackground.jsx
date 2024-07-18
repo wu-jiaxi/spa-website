ae04928432ed18a51b7df4c1b2f3ce4e58fa6e0e
wu-jiaxi <dev.jiaxiwu@gmail.com>
2024-07-18 00:46:17 -0400

    replaced video

--- src/components/VideoBackground/VideoBackground.jsx
+++ src/components/VideoBackground/VideoBackground.jsx
@@ -1,21 +1,17 @@
 import "./VideoBackground.css";
-import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import { Link } from "react-router-dom";
+import SpaLandingPhoto from "../../assets/NYC Spa_files/pexels.jpg"; // Adjust the path as necessary
 
 const VideoBackground = () => {
   return (
-    <div className="video-background">
-      <video autoPlay loop muted className="video-background__video">
-        <source src="src/assets/spa_video.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+    <div
+      className="video-background"
+      style={{ backgroundImage: `url(${SpaLandingPhoto})` }}
+    >
       <div className="video-background__content">
         <div className="navBar">
-          <MenuIcon />
           <h1>NYC Spa and Facial</h1>
-          <div></div>
         </div>
       </div>
       <div className="buttonDiv">
@@ -24,13 +20,10 @@ const VideoBackground = () => {
           variant="contained"
           style={{ borderRadius: 50 }}
         >
-          <Link to="/form" className="link">
+          <Link to="/form" className="spaHomepageButtonLink">
             Plan your Trip
           </Link>
         </Button>
-      </div>
-      <div className="downArrow">
-        <ArrowDownwardIcon />
       </div>
     </div>
   );
